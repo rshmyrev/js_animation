c1ee471319ebc63c1849c5ffb19d804a8052fe5d
Roman Shmyrev <rshmyrev@gmail.com>
2022-05-27 16:03:48 +0300

    add pause and stop

--- js/cat.js
+++ js/cat.js
@@ -2,29 +2,37 @@
 
 var animationInterval;
 var spriteSheet = document.getElementById("cat");
+const frames = 12;
+const cycleTime = 1.5; // time for full cycle
 var lengthOfSpriteSheet = 2400;
-var lengthOfEachSprite = 200;
+const lengthOfEachSprite = lengthOfSpriteSheet / frames;
+var isPaused = false;
+const speed = frames / cycleTime; // fps
+const delay = 1 / speed * 1000; //in millisecond(ms)
+console.log(delay);
 
 function stopAnimation() {
     clearInterval(animationInterval);
+    document.getElementById("stopped").style.visibility = 'visible';
 }
 
 function startAnimation() {
     var position = lengthOfEachSprite; //start position for the image
-    const speed = 100; //in millisecond(ms)
     const diff = lengthOfEachSprite; //difference between two sprites
 
     animationInterval = setInterval(() => {
-        spriteSheet.style.backgroundPosition = `0px -${position}px`;
+        if (!isPaused) {
+            spriteSheet.style.backgroundPosition = `0px -${position}px`;
 
-        if (position < lengthOfSpriteSheet) {
-            position = position + diff;
-        } else {
-            //increment the position by the width of each sprite each time
-            position = lengthOfEachSprite;
+            if (position < lengthOfSpriteSheet) {
+                position = position + diff;
+            } else {
+                //increment the position by the width of each sprite each time
+                position = lengthOfEachSprite;
+            }
+            //reset the position to show first sprite after the last one
         }
-        //reset the position to show first sprite after the last one
-    }, speed);
+    }, delay);
 }
 
 //Start animation
