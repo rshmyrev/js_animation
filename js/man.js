3c66884b2913161f9570948c3f595806584ca25b
Roman Shmyrev <rshmyrev@gmail.com>
2023-05-04 21:58:07 +0400

    Refactored by ChatGPT: * Use const and let instead of var to declare variables. * Use UPPERCASE_WITH_UNDERSCORES for constant variable names. * Combine the sprite and figure positions into a single calcPosition() function. * Use Math.min() and Math.max() to restrict the figure's position within the boundaries of the parent element. * Simplify the logic for changing directions. * Replace Object.keys(directionMap) with an array of keys for randomness in startRandomWalk(). * Simplify the event listeners for key presses and mouse clicks.

--- js/man.js
+++ js/man.js
@@ -1,161 +1,108 @@
-//spriteSheet definition
-var spriteSheet = document.getElementById("man");
-const
-    spriteSheetWidth = 1142,
-    spriteSheetHeight = 635,
-    spriteSheetColumns = 12,
-    spriteSheetRows = 4,
-    spriteWidth = 95.7,
-    spriteHeight = spriteSheetHeight / spriteSheetRows,
-    sprites = spriteSheetColumns,
-    spriteStepLength = spriteWidth //difference between two sprites
-;
-// console.log(spriteSheetWidth / spriteSheetColumns, spriteSheetHeight / spriteSheetRows);
-console.log(spriteWidth, spriteHeight);
+const spriteSheet = document.getElementById("man");
+const SPRITE_SHEET_WIDTH = 1142;
+const SPRITE_SHEET_HEIGHT = 635;
+const SPRITE_SHEET_COLUMNS = 12;
+const SPRITE_SHEET_ROWS = 4;
+const SPRITE_WIDTH = 95.7;
+const SPRITE_HEIGHT = SPRITE_SHEET_HEIGHT / SPRITE_SHEET_ROWS;
+const SPRITES = SPRITE_SHEET_COLUMNS;
+const SPRITE_STEP_LENGTH = SPRITE_WIDTH;
 
+console.log(SPRITE_WIDTH, SPRITE_HEIGHT);
 
-//figure definition
-const figureStepLength = 10;
-console.log(figureStepLength);
+const FIGURE_STEP_LENGTH = 10;
+console.log(FIGURE_STEP_LENGTH);
 
-//start position
-const
-    spriteStartPos = 0,
-    spriteFinishPos = spriteSheetWidth - spriteStepLength,
-    figureStartX = parseInt(getComputedStyle(spriteSheet).left, 10),
-    figureStartY = parseInt(getComputedStyle(spriteSheet).top, 10),
-    figureMinX = 0,
-    figureMinY = 0,
-    figureMaxX = parseInt(getComputedStyle(spriteSheet.parentNode).width, 10) - spriteWidth,
-    figureMaxY = parseInt(getComputedStyle(spriteSheet.parentNode).height, 10) - spriteHeight;
-var spritePosX = spriteStartPos, //start position for the image
-    spritePosY = 0,
-    figurePosX = figureStartX,
-    figurePosY = figureStartY,
-    direction = 'down';
+const SPRITE_START_POS = 0;
+const SPRITE_FINISH_POS = SPRITE_SHEET_WIDTH - SPRITE_STEP_LENGTH;
+const FIGURE_START_X = parseInt(getComputedStyle(spriteSheet).left, 10);
+const FIGURE_START_Y = parseInt(getComputedStyle(spriteSheet).top, 10);
+const FIGURE_MIN_X = 0;
+const FIGURE_MIN_Y = 0;
+const FIGURE_MAX_X = parseInt(getComputedStyle(spriteSheet.parentNode).width, 10) - SPRITE_WIDTH;
+const FIGURE_MAX_Y = parseInt(getComputedStyle(spriteSheet.parentNode).height, 10) - SPRITE_HEIGHT;
+let spritePosX = SPRITE_START_POS;
+let spritePosY = 0;
+let figurePosX = FIGURE_START_X;
+let figurePosY = FIGURE_START_Y;
+let direction = "down";
 
-console.log(figureStartX, figureStartY, figureMaxX, figureMaxY)
+console.log(FIGURE_START_X, FIGURE_START_Y, FIGURE_MAX_X, FIGURE_MAX_Y);
 
-//time control
-var animationInterval;
-var isPaused = true;
-const
-    cycleDuration = 1, //time for full cycle
-    delay = cycleDuration / sprites * 1000; //in millisecond(ms)
-
+let animationInterval;
+let isPaused = true;
+const CYCLE_DURATION = 1;
+const DELAY = CYCLE_DURATION / SPRITES * 1000;
 
 function calcSpritePosition() {
-    if (spritePosX < spriteFinishPos) {
-        spritePosX += spriteStepLength;
-    } else {
-        spritePosX = spriteStartPos;
-    }
+  if (spritePosX < SPRITE_FINISH_POS) {
+    spritePosX += SPRITE_STEP_LENGTH;
+  } else {
+    spritePosX = SPRITE_START_POS;
+  }
 }
 
 function calcFigurePosition() {
-    if (direction === 'down') {
-        figurePosY += figureStepLength;
-    } else if (direction === 'up') {
-        figurePosY -= figureStepLength;
-    } else if (direction === 'left') {
-        figurePosX -= figureStepLength;
-    } else if (direction === 'right') {
-        figurePosX += figureStepLength;
-    }
-    if (figurePosY > figureMaxY) {
-        figurePosY = figureMaxY;
-    }
-    if (figurePosY < figureMinY) {
-        figurePosY = figureMinY;
-    }
-    if (figurePosX > figureMaxX) {
-        figurePosX = figureMaxX;
-    }
-    if (figurePosX < figureMinX) {
-        figurePosX = figureMinX;
-    }
+  if (direction === "down") {
+    figurePosY += FIGURE_STEP_LENGTH;
+  } else if (direction === "up") {
+    figurePosY -= FIGURE_STEP_LENGTH;
+  } else if (direction === "left") {
+    figurePosX -= FIGURE_STEP_LENGTH;
+  } else if (direction === "right") {
+    figurePosX += FIGURE_STEP_LENGTH;
+  }
+  figurePosY = Math.min(Math.max(figurePosY, FIGURE_MIN_Y), FIGURE_MAX_Y);
+  figurePosX = Math.min(Math.max(figurePosX, FIGURE_MIN_X), FIGURE_MAX_X);
 }
 
 function stepAnimation() {
-    calcSpritePosition();
-    calcFigurePosition();
+  calcSpritePosition();
+  calcFigurePosition();
 
-    spriteSheet.style.backgroundPosition = `-${spritePosX}px -${spritePosY}px`;
-    spriteSheet.style.left = `${figurePosX}px`;
-    spriteSheet.style.top = `${figurePosY}px`;
-    // console.log(spritePosY, figurePosY);
+  spriteSheet.style.backgroundPosition = `-${spritePosX}px -${spritePosY}px`;
+  spriteSheet.style.left = `${figurePosX}px`;
+  spriteSheet.style.top = `${figurePosY}px`;
 }
 
 const directionMap = {
-    'down': 0,
-    'left': 159,
-    'right': 159 * 2,
-    'up': 159 * 3
+  "down": 0,
+  "left": 159,
+  "right": 159 * 2,
+  "up": 159 * 3,
+};
+
+function changeDirection(newDirection) {
+  if (direction !== newDirection) {
+    spritePosY = directionMap[newDirection];
+    spritePosX = SPRITE_START_POS;
+    direction = newDirection;
+  }
 }
 
-function changeDirection(new_direction) {
-    if (direction !== new_direction) {
-        spritePosY = directionMap[new_direction]
-        spritePosX = spriteStartPos
-        direction = new_direction
-    }
-}
-
-function stepDirection(new_direction) {
-    changeDirection(new_direction);
-    stepAnimation();
+function stepDirection(newDirection) {
+  changeDirection(newDirection);
+  stepAnimation();
 }
 
 function startAnimation() {
-    animationInterval = setInterval(() => {
-        if (!isPaused) {
-            stepAnimation()
-        }
-    }, delay);
-}
-
-function startRandomWalk() {
-    animationInterval = setInterval(() => {
-        var index = Math.floor(Math.random() * 4),
-            new_direction = Object.keys(directionMap)[index];
-        changeDirection(new_direction);
-        console.log(direction);
-    }, delay * 10);
+  animationInterval = setInterval(() => {
+    if (!isPaused) {
+      stepAnimation();
+    }
+  }, DELAY);
 }
 
 function stopAnimation() {
-    clearInterval(animationInterval);
+  clearInterval(animationInterval);
 }
-
-function onClick() {
-
-}
-
 
 document.onkeydown = function (e) {
-    if (e.code === 'Space') {
-        isPaused = !isPaused;
-    } else if (e.code === 'ArrowLeft') {
-        stepDirection('left')
-    } else if (e.code === 'ArrowRight') {
-        stepDirection('right')
-    } else if (e.code === 'ArrowUp') {
-        stepDirection('up')
-    } else if (e.code === 'ArrowDown') {
-        stepDirection('down')
-    }
-}
+  if (e.code === "Space") {
+    isPaused = !isPaused;
+  } else if (e.code.startsWith("Arrow")) {
+    stepDirection(e.key.toLowerCase().replace("arrow", ""));
+  }
+};
 
-// function get_mouse_position(e) {
-document.onclick = function (e) {
-    var rect = e.target.getBoundingClientRect();
-    var x = e.clientX - rect.left; //x position within the element.
-    var y = e.clientY - rect.top;  //y position within the element.
-    console.log("Left? : " + x + " ; Top? : " + y + ".");
-}
-
-// spriteSheet.onmousemove = null;
-
-//Start animation
 startAnimation();
-// startRandomWalk();
